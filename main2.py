0348494cfb400035333059ae69c0ab43f10f1fdb
rzgeer <67606150+rzgeer@users.noreply.github.com>
2021-02-01 16:12:13 -0500

    Completes assignment

--- main2.py
+++ main2.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-import sys
+import sys, random
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+number = random.randrange(1,11)
+count = 0
+guess = -1
+while guess != number:
+    count = count + 1
+    guess = input("Guess a number from 1 to 10: ")
+    try:
+        guess = int(guess)
+        if guess != number:
+            print("Not quite, please try again!")
+    except:
+        print("Please enter a number!")
+print("Great job! You got it! You were able to guess the number in " + str(count) + " tries.")
